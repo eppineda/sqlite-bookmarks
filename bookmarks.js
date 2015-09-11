54f01273b3d5d824709490273940a8acec7edb06
Enrique Pineda <enrique.pineda@gmail.com>
2015-09-11 02:15:28 -0400

    db.export() not db.exportSQLite()

--- bookmarks.js
+++ bookmarks.js
@@ -5,27 +5,8 @@ function exportSQLite(db) {
         return String.fromCharCode.apply(String, array)
     }
 
-    return simpleStorage.set(constants.mybookmarks,
-        bin2String(db.exportSQLite())
-    ) /* true, false, or error object */
-
-}
-
-function importLocalStorage() {
-    var string2Bin = function(str) {
-        return str.split("").map( function(val) {
-            return val.charCodeAt(0)
-        } )
-    }
-    var datastring = simpleStorage.get(constants.mybookmarks)
-
-    if ('undefined' !== datastring) {
-        var data = string2Bin(datastring)
-        // todo: complete
-    }
-    else {
-        // todo: something went wrong; no data retrieved
-    }
+    return simpleStorage.set(constants.mybookmarks, bin2String(db.export()))
+        /* true, false, or error object */
 }
 
 function createDB() {
@@ -49,5 +30,22 @@ function createDB() {
     else {
         throw { name:'BookmarkException', msg:'local storage not an option' }
         db.close()
+    }
+}
+
+function importLocalStorage() {
+    var string2Bin = function(str) {
+        return str.split("").map(function(val) {
+            return val.charCodeAt(0)
+        })
+    }
+    var datastring = simpleStorage.get(constants.mybookmarks)
+
+    if ('undefined' !== datastring) {
+        var data = string2Bin(datastring)
+        // todo: complete
+    }
+    else {
+        // todo: something went wrong; no data retrieved
     }
 }
