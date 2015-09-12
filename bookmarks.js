ae716fdf6cbe4e4861d775ef3e83c989b248bf1b
Enrique Pineda <enrique.pineda@gmail.com>
2015-09-11 21:42:41 -0400

    automatically initialize SQLite, either by restoring from local storage, or by creating anew.

--- bookmarks.js
+++ bookmarks.js
@@ -1,4 +1,5 @@
 var constants = { 'mybookmarks':'mybookmarks' }
+var db
 
 function exportSQLite(db) {
     var bin2String = function(array) {
@@ -8,6 +9,18 @@ function exportSQLite(db) {
     return simpleStorage.set(constants.mybookmarks, bin2String(db.export()))
         /* true, false, or error object, according to API docs */
 }
+
+function importLocalStorage() {
+    var string2Bin = function(str) {
+        return str.split("").map(function(val) {
+            return val.charCodeAt(0)
+        })
+    }
+    var datastring = simpleStorage.get(constants.mybookmarks)
+
+    return ('undefined' === typeof datastring ? datastring : string2Bin(datastring))
+        /* either undefined or array of binary */
+} // importLocalStorage
 
 function createDB() {
     if (!simpleStorage.canUse())
@@ -39,29 +52,27 @@ function saveDB(db) {
     var result = exportSQLite(db)
 
     if (false === result)
-        throw { name:'BookmarkException', msg:'failed to write to local storage'}
+        throw { name:'BookmarkException', msg:'failed to write to local storage' }
     if ('Object' === typeof result)
         throw result // something very bad happened
 // successfully written to local storage
 } // saveDB
 
-function importLocalStorage() {
-    var string2Bin = function(str) {
-        return str.split("").map(function(val) {
-            return val.charCodeAt(0)
-        })
-    }
-    var datastring = simpleStorage.get(constants.mybookmarks)
+function restoreDB() {
+    if (!simpleStorage.canUse())
+        throw { name:'BookmarkException', msg:'local storage not an option' }
 
-    if ('undefined' !== datastring) {
-        var data = string2Bin(datastring)
-        // todo: complete
-    }
-    else {
-        // todo: something went wrong; no data retrieved
-    }
-} // importLocalStorage
+    var data = importLocalStorage()
 
-function saveDB(db) {
-    // todo
+    return ('undefined' === typeof data ? createDB() : new SQL.Database(data))
+        /* return reference to SQLite database in memory */
 }
+
+(function initialize() {
+    try {
+        db = restoreDB()
+    }
+    catch(e) {
+        console.error(e)
+    }
+})()
